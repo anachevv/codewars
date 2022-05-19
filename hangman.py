5faa2189ab9f275f1579abdfa9a28dee27bdf201
Anatoli Nachev <36029469+CodingPawn@users.noreply.github.com>
2022-05-19 23:29:14 +0300

    Quick update

--- hangman.py
+++ hangman.py
@@ -1,6 +1,6 @@
 import random
 
-mylist = ["secret", "fridge", "programmer", "bank", "money", "music", "security"]
+mylist = ("secret", "fridge", "programmer", "bank", "money", "music", "security")
 word = random.choice(mylist)
 print("Player 1 - Pick a word; Player 2 - Guess it!")
 print("I choose this one:")
@@ -10,11 +10,11 @@ done = False
 while not done:
     for letter in word:
         if letter.lower() in guesses:
-            print(letter, end = " ")
+            print(letter, end=" ")
         else:
-            print("_", end = " ")
+            print("_", end=" ")
     print("")
-    guess = input(f"Allowed errors left {allowed_errors}, next guess: ")
+    guess = input(f"Allowed errors left {allowed_errors}, next guess:\n ")
     guesses.append(guess.lower())
     if guess.lower() not in word.lower():
         allowed_errors -= 1
@@ -28,6 +28,6 @@ while not done:
 
 if done:
     print({word})
-    print("You guessed correctly! It was " + word)
+    print(f"You guessed correctly! It was '{word}'.")
 else:
-    print("Game Over! The word was " + word)
+    print(f"Game Over! The word was '{word}'.")
