9713cf0125006c32cf3b022558b7f3b119d393b8
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-07-05 15:08:22 +0300

    Server added to the app.

--- bmi-calculator/bmi_calculator.py
+++ bmi-calculator/bmi_calculator.py
@@ -1,33 +1,26 @@
+from pywebio import start_server
 from pywebio.input import input, FLOAT
 from pywebio.output import put_text
 
 
-def bmi_calculator():
+def main():  # PyWebIO application function
+    def bmi_calculator():
+        height = input('Input your height (cm):', type=FLOAT, required=True, placeholder='Your height')
+        weight = input('Input your weight (kg):', type=FLOAT, required=True, placeholder='Your weight')
 
-    # Taking user's height and weight.
+        bmi = weight / (height / 100) ** 2
 
-    height = input('Input your height (cm):', type=FLOAT, required=True, placeholder='Your height')
-    weight = input('Input your weight (kg):', type=FLOAT, required=True, placeholder='Your weight')
+        top_status = [(16, 'Severely underweight'), (18.5, 'Underweight'),
+                      (25, 'Normal'), (30, 'Overweight'),
+                      (35, 'Moderately obese'), (float('inf'), 'Severely obese')]
 
-    # Calculating user's BMI
+        for top, top_status in top_status:
+            if bmi <= top:
+                put_text(f'Your BMI = {bmi:.1f} --> Category: {top_status}')
+                break
 
-    bmi = weight / (height / 100) ** 2
-
-    # BMI result column
-
-    top_status = [(16, 'Severely underweight'), (18.5, 'Underweight'),
-                  (25, 'Normal'), (30, 'Overweight'),
-                  (35, 'Moderately obese'), (float('inf'), 'Severely obese')]
-
-    # Printing user's BMI result and its certain category
-
-    for top, top_status in top_status:
-        if bmi <= top:
-            put_text(f'Your BMI = {bmi:.1f} --> Category: {top_status}')
-            break
+    if __name__ == '__main__':
+        bmi_calculator()
 
 
-# Running the script
-
-if __name__ == '__main__':
-    bmi_calculator()
+start_server(main, port=8080, debug=True)
