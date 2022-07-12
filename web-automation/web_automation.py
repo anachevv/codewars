2dcfcc1ed3762e3a49846f37337fa24c115e03dc
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-07-12 23:37:41 +0300

    quick update

--- web-automation/web_automation.py
+++ web-automation/web_automation.py
@@ -3,7 +3,6 @@ import webbrowser  # Import webbrowser library
 
 def web_automation():  # Creating a function
     urls = ("stackoverflow.com", "github.com", "neuralnine.com")  # URLs we want to reach
-
     for url in urls:  # Creating a for-loop in order to open any URL at once.
         webbrowser.open("https://" + url)  # As we do not use a specific directory of Chrome, for example, we add
         # 'https://' to the URL to open it on default browser.
