02d11587185c503795c164731ce29338a5b96a13
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2023-02-19 12:01:09 +0200

    solution added

---
+++ HighestAndLowest/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(highAndLow("6"));
+        System.out.println(highAndLow("30"));
+        System.out.println(highAndLow("-23"));
+        System.out.println(highAndLow("-5"));
+        System.out.println(highAndLow("1 2 3 4 5"));
+        System.out.println(highAndLow("1 2 -3 4 5"));
+        System.out.println(highAndLow("1 9 3 4 -5"));
+        System.out.println(highAndLow("-5 -3 -9 41 40 42"));
+    }
+    public static String highAndLow(String numbers) {
+        String[] strArr = numbers.split(" ");
+        int[] arr = new int[strArr.length];
+
+        for(int i = 0;i < strArr.length;i++) {
+            arr[i] = Integer.parseInt(strArr[i]);
+        }
+
+        if (arr.length == 1) {
+            return String.format("%d %d", arr[0], arr[0]);
+        }
+
+        int max = Arrays.stream(arr).max().orElse(0);
+        int min = Arrays.stream(arr).min().orElse(0);
+        return String.format("%d %d", max, min);
+    }
+}
